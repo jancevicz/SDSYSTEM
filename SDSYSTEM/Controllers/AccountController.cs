8d87d26447a1471b04368677b73b0f1f7e00f956
Przemek <jancewicz.przemek@gmail.com>
2024-06-08 16:07:54 +0200

    Homepage

---
+++ SDSYSTEM/Controllers/AccountController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SDSYSTEM.Data;
+using SDSYSTEM.Models;
+using System.Threading.Tasks;
+
+namespace SDSYSTEM.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public AccountController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Account/Login
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        // POST: Account/Login
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Login(LoginViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                // Implement your login logic here
+                // Example: Check user credentials, set authentication cookie, etc.
+                // Redirect to appropriate page after login
+                return RedirectToAction("Index", "Home");
+            }
+            return View(model);
+        }
+    }
+}
