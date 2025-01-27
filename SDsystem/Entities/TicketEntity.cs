591678bb571cc0208cbb311b7ba1c4914ff8c22a
Przemek <jancewicz.przemek@gmail.com>
2025-01-27 22:19:25 +0100

    eng to pl

--- SDsystem/Entities/TicketEntity.cs
+++ SDsystem/Entities/TicketEntity.cs
@@ -1,15 +1,22 @@
-﻿namespace SDsystem.Entities
+﻿using System.ComponentModel.DataAnnotations;
+namespace SDsystem.Entities
 
 {
     public class TicketEntity
     {
         public int Id { get; set; }
+        [Display(Name = "Tytuł")]
         public string Subject { get; set; }
-        public string? Description { get; set; }   
-        public string Name { get; set; }    
+        [Display(Name = "Opis")]
+        public string? Description { get; set; }
+        [Display(Name = "Imię")]
+        public string Name { get; set; }
+        [Display(Name = "Nazwisko")]
         public string Surname { get; set; }
+        [Display(Name = "Dział")]
         public string Department { get; set; }
         public string Status { get; set; } = "Aktywne";
+        [Display(Name = "Data")]
         public DateTime Date { get; set; } = DateTime.Now;
 
     }
